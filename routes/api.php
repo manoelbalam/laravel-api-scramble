78f47e16722e72929bc527b825af0d8f20e83e28
isaac balam <manoel.balam@gmail.com>
2025-08-03 01:33:50 -0500

    add authorization header

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,11 @@
 use App\Http\Controllers\API\AuthenticationController;
 use App\Http\Controllers\PostController;
 
-Route::post('register', [AuthenticationController::class, 'register']);
-Route::post('login', [AuthenticationController::class, 'login']);
+
+Route::prefix('v1')->group(function () {
+    Route::post('register', [AuthenticationController::class, 'register']);
+    Route::post('login', [AuthenticationController::class, 'login']);
+});
 
 Route::middleware('auth:sanctum')->prefix('v1')->group(function () {
     Route::get('user', [AuthenticationController::class, 'userInfo']);
